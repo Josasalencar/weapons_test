36b33476fc605834ff6b15822fadf75486fa735e
josa_ <josasalencar@gmail.com>
2022-05-13 22:42:09 -0300

    fixing endpoints

--- mysite/weapons/serializers.py
+++ mysite/weapons/serializers.py
@@ -1,12 +1,25 @@
 from rest_framework import serializers
 from .models import Arma, Municao
+from .models import Objeto_tipo,Objeto
 
 class ArmaSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Arma
-        fields = '__all__'
+        fields =['id_arma',
+                'calibre',
+                'marca',
+                'modelo',
+                'quantidade_de_tiros',
+                'valor_estimado',
+                'imagem']
+
 
 class MunicaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Municao
-        fields = '__all__'
+        fields = ['id_municao',
+                'calibre',
+                'marca',
+                'modelo',
+                'valor_estimado']
